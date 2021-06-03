734e5e62204a6627b0d0678fe30e2e64fb22f638
Lakkie <lakkie@lakkie.net>
2021-06-03 12:20:48 -0700

    Modified snake settings

--- snake/src/game/GameSettings.java
+++ snake/src/game/GameSettings.java
@@ -4,9 +4,11 @@ import java.util.Random;
 
 public class GameSettings {
     
-    public static final long UPDATE_RATE = 30;
+    public static final long UPDATE_RATE = 15;
 
-    public static final int GRID_WIDTH = 50, GRID_HEIGHT = 50 / 16 * 9;
+    public static final int GRID_SCALE = 50;
+
+    public static final int GRID_WIDTH = GRID_SCALE, GRID_HEIGHT = GRID_SCALE / 16 * 9;
 
     public static final Random RANDOM = new Random();
 
